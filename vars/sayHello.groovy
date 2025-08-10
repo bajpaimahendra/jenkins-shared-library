c23e80fc3570d3132ff3d58471a073b066c49210
Mahendra <mbajpai@contecglobal.com>
2025-08-10 10:24:27 +0530

    okay

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,3 +1,12 @@
 def call(String name = 'World') {
     echo "Hello, ${name}! Welcome to Jenkins Shared Libraries."
 }
+
+// Named helpers: call as devOps.buildImage(), devOps.pushImage()
+def buildImage(String tag) {
+  sh "docker build -t ${tag} ."
+}
+
+def showAllContainers(String containerName) {
+  sh "docker inspect ${containerName} ."
+}
